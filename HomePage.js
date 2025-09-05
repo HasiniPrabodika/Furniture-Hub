735d305bd62b38966fa6a3b7585a498591914db7
Hasini Prabodhika <ar112724@fhss.sjp.ac.lk>
2025-09-05 18:03:55 +0530

    Delete HomePage.js

--- HomePage.js
+++
@@ -1,6 +0,0 @@
-
-window.location.href = 'Review.html';
-window.location.href = 'HomePage.html';
-window.location.href = 'ProductPage.html';
-window.location.href = 'ContactPage.html';
-window.location.href = 'About.html';
