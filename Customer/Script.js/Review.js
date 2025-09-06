92cbaeeacb388d4e85f4206fbb03bab9793abc73
Shehani28 <ar112770@fhss.sjp.ac.lk>
2025-09-06 14:17:57 +0530

    Review page with button navigation

--- Customer/Script.js/Review.js
+++ Customer/Script.js/Review.js
@@ -1,3 +1,6 @@
-function goToReviewPage() {
-    window.location.href = '../HTML/Review.html'; 
-}
+
+window.location.href = 'Review.html';
+window.location.href = 'HomePage.html';
+window.location.href = 'ProductPage.html';
+window.location.href = 'ContactPage.html';
+window.location.href = 'About.html';
