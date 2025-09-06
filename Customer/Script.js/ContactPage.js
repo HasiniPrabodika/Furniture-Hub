efc19ce29ad2f389b2648dd48cc9beca26d4de8b
nimalinimesha <ar112382@fhss.sjp.ac.lk>
2025-09-06 09:20:53 +0530

    install express packej

--- Customer/Script.js/ContactPage.js
+++ Customer/Script.js/ContactPage.js
@@ -1,8 +1,1 @@
-const navLinks = document.querySelectorAll('.navbar a');
 
-navLinks.forEach(link => {
-  link.addEventListener('click', function () {
-    navLinks.forEach(l => l.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
