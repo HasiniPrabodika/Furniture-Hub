c8382a64ae06d4d0510348a467cc5f4ffc9ee4eb
Shehani28 <ar112770@fhss.sjp.ac.lk>
2025-09-08 14:49:52 +0530

    Connect database to Additem page

--- Admin/Script.js/AddItem.js
+++ Admin/Script.js/AddItem.js
@@ -1,6 +1,44 @@
+const form = document.getElementById("furnitureForm");
 
-window.location.href = "../../Customer/HTML/HomePage.html";
-window.location.href = "../../Customer/HTML/ProductPage.html";
-window.location.href = "../../Customer/HTML/Review.html";
-window.location.href = "../../Customer/HTML/ContactPage.html";
-window.location.href = "../../Customer/HTML/About.html";
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); 
+
+
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const category = document.getElementById("category").value;
+  const description = document.getElementById("description").value;
+  const image = document.getElementById("image").files[0];
+
+  if (!image) {
+    alert("Please select an image!");
+    return;
+  }
+
+  
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("price", price);
+  formData.append("category", category);
+  formData.append("description", description);
+  formData.append("image", image);
+
+  try {
+    const response = await fetch("http://localhost:5000/api/items", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Item added successfully ✅");
+      form.reset();
+    } else {
+      alert("Error adding item: " + data.message);
+    }
+  } catch (err) {
+    console.error(err);
+    alert("Server error. Check console.");
+  }
+});
