59f3a2a40a4a7509e4c7a0938c6c7f0d3a9d6dfb
Dasuni Ekanayaka <ar112301@fhss.sjp.ac.lk>
2025-09-08 23:21:21 +0530

    Add and update  files

--- Admin/Script.js/Admin login.js
+++ Admin/Script.js/Admin login.js
@@ -5,3 +5,39 @@ window.location.href = "../../Customer/HTML/ContactPage.html";
 window.location.href = "../../Customer/HTML/About.html";
 
 
+var lblUsername=document.getElementById("lbluser");
+var lblpassword=document.getElementById("lblpass");
+var txtUsername=document.getElementById("username");
+var txtpassword=document.getElementById("password");
+
+lblUsername.addEventListener("click",function(){
+    lblUsername.classList.add("small");
+})
+
+lblpassword.addEventListener("click",function(){
+    lblpassword.classList.add("small");
+})
+txtUsername.addEventListener("click",function(){
+    lblUsername.classList.add("small");
+})
+
+txtpassword.addEventListener("click",function(){
+    lblpassword.classList.add("small");
+})
+
+txtUsername.addEventListener("blur",function(){
+    if (txtUsername.value =='' ){
+        lblUsername.classList.remove("small");
+    }
+
+});
+  
+
+txtpassword.addEventListener("blur",function(){
+    if (txtpassword.value =='' ){
+        lblpassword.classList.remove("small");
+    }
+   
+});
+
+
--- server.js
+++ server.js
@@ -1,22 +1,84 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+
+
+const Furniture = require('./models/Furniture'); 
 
 const app = express();
-const PORT = 5000; // already using 5000
-
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); 
 
-// MongoDB connection
-const mongoURI = "mongodb+srv://hprabodika657_db_user:64zCPMGuNJEb0Y7P@cluster0.r3sjfra.mongodb.net/CraftifyDB?retryWrites=true&w=majority";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+const reviewRoutes = require('./routes/reviewRoutes'); 
+app.use('/api/reviews', reviewRoutes);
+
+
+
+
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads'); 
+    },
+    filename: (req, file, cb) => {
+       
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+
+
+
+
+
+app.post('/add-item', upload.single('image'), (req, res) => {
+    
+    const { name, price, category, description } = req.body;
+    
+    
+    const imagePath = req.file ? '/uploads/' + req.file.filename : null; 
+
+    const newFurniture = new Furniture({
+        name,
+        price,
+        category,
+        image: imagePath, 
+        description
+    });
+
+    newFurniture.save()
+        .then(() => {
+            
+            res.redirect('/dashboard');
+        })
+        .catch(err => {
+            console.error('Error for Saving:', err);
+            res.status(500).send('Error for Saving.');
+        });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.get('/dashboard', (req, res) => {
+    Furniture.find({})
+        .then(furnitureItems => {
+            res.json(furnitureItems); 
+        })
+        .catch(err => {
+            console.error('Error fetching furniture items:', err);
+            res.status(500).send('Error fetching furniture items.');
+        });
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
