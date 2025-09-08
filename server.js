9b89ce16a8729ddcefae2e7c3656cb8e66111f3f
Hasini Prabodhika <ar112724@fhss.sjp.ac.lk>
2025-09-08 22:30:26 +0530

    Update delete button style in the Admin Dashboard page and website contact email

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connect
+const mongoURI =  "mongodb+srv://hprabodika657_db_user:64zCPMGuNJEb0Y7P@cluster0.r3sjfra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB connection error:", err));
+
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
